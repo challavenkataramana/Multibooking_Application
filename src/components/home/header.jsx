d98bf11c4a53eab50fe7cef16c7fb92f43d5e520
challavenkataramana <challavenkataramana@gmail.com>
2025-02-08 23:26:45 +0530

    Make the app name clickable to navigate to the Home page and add hover effect

--- src/components/home/header.jsx
+++ src/components/home/header.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import {Logout} from './Logout/logout';
+import { useNavigate } from "react-router-dom";
+
 export const Header = ({ handleLogout }) => {
+    
+     const navigate = useNavigate();
+    const handleroute=()=>{
+        navigate("/Home");
+    }
     return (
         <header className="header">
-            <div className="app-name">Booking Portal</div>
+            <div className="app-name" onClick={handleroute}>Booking Portal</div>
             <nav className="nav-buttons">
                 <Logout handleLogout={handleLogout} />    
             </nav>
